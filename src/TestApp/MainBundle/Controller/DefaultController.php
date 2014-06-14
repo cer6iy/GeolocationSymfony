64ae224bc107c0a5313764323daad7dd4ae77d98
DeSergo <cer6iy@outlook.com>
2014-06-14 14:03:10 +0700

    Unstable first version

--- src/TestApp/MainBundle/Controller/DefaultController.php
+++ src/TestApp/MainBundle/Controller/DefaultController.php
@@ -4,15 +4,59 @@ namespace TestApp\MainBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Httpful\Request;
+use TestApp\MainBundle\Entity\Contact;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $uri = "http://api.sypexgeo.net";
+        $defaultCity = "Moscow";
 
-        $buzz = $this->container->get('buzz');
-        $output = $buzz->get('http://api.sypexgeo.net/');
+        // get city by ip
+        $response = Request::get($uri)
+            ->expectsJson()
+            ->sendIt();
+        $currentCity = $response->body->city->name_en;
 
-        return new Response('<html><body>' . $output . '</body></html>');
+        $repository = $this->getDoctrine()->getRepository('TestAppMainBundle:Contact');
+        //get contact by city english name
+        $contact = $repository->findOneBy (array('city_en' => $currentCity));
+        if (!$contact) {
+            $contact = $repository->findOneBy (array('city_en' => $defaultCity));
+        }
+
+        /*return new Response('<html><body> Created product id ' . $contact->getId() . $response->body->ip . "  " . $response->body->city->name_en . "  " . $response->body->city->name_ru . '</body></html>');*/
+
+
+        return $this->redirect(strtolower ($contact->getCityEn()));
+    }
+
+
+
+
+    public function contactAction($city)
+    {
+        $defaultCity = "Moscow";
+
+        // get city by ip
+
+        $currentCity = $city;
+
+        $repository = $this->getDoctrine()->getRepository('TestAppMainBundle:Contact');
+        //get contact by city english name
+        $contact = $repository->findOneBy (array('city_en' => $currentCity));
+        if (!$contact) {
+            return $this->redirect(strtolower ($defaultCity));
+        }
+
+        $contacts = $repository->findAll();
+
+        return $this->render('TestAppMainBundle:Default:index.html.twig', array('contacts' => $contacts, 'contact' => $contact));
+
+        /*return new Response('<html><body> Created product id ' . $contact->getId() . $response->body->ip . "  " . $response->body->city->name_en . "  " . $response->body->city->name_ru . '</body></html>');*/
+
+        //return new Response('<html><body> Добрый день, вы просматриваете страницу для города ' . $contact->getCityRu() .', телефон для справок: ' . $contact->getPhone() . '</body></html>');
     }
 }
